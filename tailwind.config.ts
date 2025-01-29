c3234298d4196ac4c6d9f32cce980210188b93f2
Hadia Khalil <168618032+HadiaKhalil@users.noreply.github.com>
2025-01-29 13:37:48 +0500

    add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
+   
+    "./src/Component/**/*.{js,ts,jsx,tsx,mdx}",
   
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
